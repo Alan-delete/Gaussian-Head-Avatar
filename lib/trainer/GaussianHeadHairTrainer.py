d477d0e0b73105048923e2b6f2cd6e00c5ab0626
Alan-delete <1155124491@link.cuhk.edu.hk>
2025-03-04 18:32:12 +0100

    Further complete hair gaussian

---
+++ lib/trainer/GaussianHeadHairTrainer.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+import random
+import lpips
+
+
+class GaussianHeadHairTrainer():
+    def __init__(self, dataloader, delta_poses, gaussianhead, gaussianhair,supres, camera, optimizer, recorder, gpu_id, cfg = None):
+        self.dataloader = dataloader
+        self.delta_poses = delta_poses
+        self.gaussianhead = gaussianhead
+        self.gaussianhair = gaussianhair
+        self.supres = supres
+        self.camera = camera
+        self.optimizer = optimizer
+        self.recorder = recorder
+        self.device = torch.device('cuda:%d' % gpu_id)
+        self.fn_lpips = lpips.LPIPS(net='vgg').to(self.device)
+        # TODO: check which member of cfg is used in the code and only pass those
+        self.cfg = cfg
+
+    def train(self, start_epoch=0, epochs=1):
+        for epoch in range(start_epoch, epochs):
+            for idx, data in tqdm(enumerate(self.dataloader)):
+                
+                # prepare data
+                to_cuda = ['images', 'masks', 'visibles', 'images_coarse', 'masks_coarse', 'visibles_coarse', 
+                           'intrinsics', 'extrinsics', 'world_view_transform', 'projection_matrix', 'full_proj_transform', 'camera_center',
+                           'pose', 'scale', 'exp_coeff', 'landmarks_3d', 'exp_id']
+                for data_item in to_cuda:
+                    data[data_item] = data[data_item].to(device=self.device)
+
+                images = data['images']
+                visibles = data['visibles']
+                if self.supres is None:
+                    images_coarse = images
+                    visibles_coarse = visibles
+                else:
+                    images_coarse = data['images_coarse']
+                    visibles_coarse = data['visibles_coarse']
+
+                resolution_coarse = images_coarse.shape[2]
+                resolution_fine = images.shape[2]
+
+                data['pose'] = data['pose'] + self.delta_poses[data['exp_id'], :]
+
+                # render coarse images
+                head_data = self.gaussianhead.generate(data)
+                hair_data = self.gaussianhair.generate(data)
+                # combine head and hair data
+                for key in ['xyz', 'color', 'scales', 'rotation', 'opacity']:
+                    # first dimension is batch size, concat along the second dimension
+                    data[key] = torch.cat([head_data[key], hair_data[key]], dim=1)
+
+                data = self.camera.render_gaussian(data, resolution_coarse)
+                viewspace_point_tensor = data["viewspace_points"]
+                visibility_filter = data["visibility_filter"]
+                radii = data["radii"]
+                render_images = data['render_images']
+
+                # crop images for augmentation
+                scale_factor = random.random() * 0.45 + 0.8
+                scale_factor = int(resolution_coarse * scale_factor) / resolution_coarse
+                cropped_render_images, cropped_images, cropped_visibles = self.random_crop(render_images, images, visibles, scale_factor, resolution_coarse, resolution_fine)
+                data['cropped_images'] = cropped_images
+                
+                # generate super resolution images
+                supres_images = self.supres(cropped_render_images) if self.supres else cropped_render_images
+                data['supres_images'] = supres_images
+
+                # loss functions
+                loss_rgb_lr = F.l1_loss(render_images[:, 0:3, :, :] * visibles_coarse, images_coarse * visibles_coarse)
+                loss_rgb_hr = F.l1_loss(supres_images * cropped_visibles, cropped_images * cropped_visibles)
+                left_up = (random.randint(0, supres_images.shape[2] - 512), random.randint(0, supres_images.shape[3] - 512))
+                loss_vgg = self.fn_lpips((supres_images * cropped_visibles)[:, :, left_up[0]:left_up[0]+512, left_up[1]:left_up[1]+512], 
+                                            (cropped_images * cropped_visibles)[:, :, left_up[0]:left_up[0]+512, left_up[1]:left_up[1]+512], normalize=True).mean()
+                loss = loss_rgb_hr + loss_rgb_lr + loss_vgg * 1e-1
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                # Densification
+                if self.cfg.densify:
+                    with torch.no_grad():
+                        opacity_reset_interval = 3000
+                        densify_from_iter = 500 
+                        densify_until_iter = 15_000
+                        densify_grad_threshold = 0.0002
+                        opacity_reset_interval = 3_000
+                        # TODO: By printing the value of Gaussian Hair cut. Need to get this value in this project
+                        cameras_extent = 4.907987451553345
+                        iteration = idx + epoch * len(self.dataloader)
+                        if iteration <= densify_until_iter :
+                            # Keep track of max radii in image-space for pruning
+                            # TODO: visibility_filter and radii here is batched(with batchsize=1), confict with the original code
+                            unstrct_gaussian_num = self.gaussianhead.xyz.shape[0]
+                            # [B, total_gaussian_num] -> [unstruct_gaussian_num]
+                            visibility_filter = visibility_filter[0][:unstrct_gaussian_num]
+                            radii = radii[0][:unstrct_gaussian_num]
+                            self.gaussianhead.max_radii2D[visibility_filter] = torch.max(self.gaussianhead.max_radii2D[visibility_filter], radii[visibility_filter])
+                            self.gaussianhead.add_densification_stats(viewspace_point_tensor, visibility_filter)
+
+                            if iteration >= densify_from_iter and iteration % 2000 == 0:
+                                size_threshold = 20 if iteration > opacity_reset_interval else None
+                                self.gaussianhead.densify_and_prune(densify_grad_threshold, 0.005, cameras_extent, size_threshold)
+                            
+                            if iteration % opacity_reset_interval == 0 :
+                                self.gaussianhead.reset_opacity()
+
+                log = {
+                    'data': data,
+                    'delta_poses' : self.delta_poses,
+                    'gaussianhead' : self.gaussianhead,
+                    'gaussianhair' : self.gaussianhair,
+                    'supres' : self.supres,
+                    'loss_rgb_lr' : loss_rgb_lr,
+                    'loss_rgb_hr' : loss_rgb_hr,
+                    'loss_vgg' : loss_vgg,
+                    'epoch' : epoch,
+                    'iter' : idx + epoch * len(self.dataloader)
+                }
+                self.recorder.log(log)
+
+
+    def random_crop(self, render_images, images, visibles, scale_factor, resolution_coarse, resolution_fine):
+        render_images_scaled = F.interpolate(render_images, scale_factor=scale_factor)
+        images_scaled = F.interpolate(images, scale_factor=scale_factor)
+        visibles_scaled = F.interpolate(visibles, scale_factor=scale_factor)
+
+        if scale_factor < 1:
+            render_images = torch.ones([render_images_scaled.shape[0], render_images_scaled.shape[1], resolution_coarse, resolution_coarse], device=self.device)
+            left_up_coarse = (random.randint(0, resolution_coarse - render_images_scaled.shape[2]), random.randint(0, resolution_coarse - render_images_scaled.shape[3]))
+            render_images[:, :, left_up_coarse[0]: left_up_coarse[0] + render_images_scaled.shape[2], left_up_coarse[1]: left_up_coarse[1] + render_images_scaled.shape[3]] = render_images_scaled
+
+            images = torch.ones([images_scaled.shape[0], images_scaled.shape[1], resolution_fine, resolution_fine], device=self.device)
+            visibles = torch.ones([visibles_scaled.shape[0], visibles_scaled.shape[1], resolution_fine, resolution_fine], device=self.device)
+            left_up_fine = (int(left_up_coarse[0] * resolution_fine / resolution_coarse), int(left_up_coarse[1] * resolution_fine / resolution_coarse))
+            images[:, :, left_up_fine[0]: left_up_fine[0] + images_scaled.shape[2], left_up_fine[1]: left_up_fine[1] + images_scaled.shape[3]] = images_scaled
+            visibles[:, :, left_up_fine[0]: left_up_fine[0] + visibles_scaled.shape[2], left_up_fine[1]: left_up_fine[1] + visibles_scaled.shape[3]] = visibles_scaled
+        else:
+            left_up_coarse = (random.randint(0, render_images_scaled.shape[2] - resolution_coarse), random.randint(0, render_images_scaled.shape[3] - resolution_coarse))
+            render_images = render_images_scaled[:, :, left_up_coarse[0]: left_up_coarse[0] + resolution_coarse, left_up_coarse[1]: left_up_coarse[1] + resolution_coarse]
+
+            left_up_fine = (int(left_up_coarse[0] * resolution_fine / resolution_coarse), int(left_up_coarse[1] * resolution_fine / resolution_coarse))
+            images = images_scaled[:, :, left_up_fine[0]: left_up_fine[0] + resolution_fine, left_up_fine[1]: left_up_fine[1] + resolution_fine]
+            visibles = visibles_scaled[:, :, left_up_fine[0]: left_up_fine[0] + resolution_fine, left_up_fine[1]: left_up_fine[1] + resolution_fine]
+        
+        return render_images, images, visibles
---
+++ train_gaussianheadhair.py
@@ -0,0 +1,102 @@
+import os
+import torch
+import argparse
+
+from config.config import config_train
+
+from lib.dataset.Dataset import GaussianDataset
+from lib.dataset.DataLoaderX import DataLoaderX
+from lib.module.MeshHeadModule import MeshHeadModule
+from lib.module.GaussianHeadModule import GaussianHeadModule
+from lib.module.GaussianHairModule import GaussianHairModule
+from lib.module.SuperResolutionModule import SuperResolutionModule
+from lib.module.CameraModule import CameraModule
+from lib.recorder.Recorder import GaussianHeadTrainRecorder
+from lib.trainer.GaussianHeadTrainer import GaussianHeadTrainer
+from lib.trainer.GaussianHeadHairTrainer import GaussianHeadHairTrainer
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='config/train_s2_N031.yaml')
+    arg = parser.parse_args()
+
+    cfg = config_train()
+    cfg.load(arg.config)
+    cfg = cfg.get_cfg()
+
+    dataset = GaussianDataset(cfg.dataset)
+    dataloader = DataLoaderX(dataset, batch_size=cfg.batch_size, shuffle=True, pin_memory=True) 
+
+    device = torch.device('cuda:%d' % cfg.gpu_id)
+    torch.cuda.set_device(cfg.gpu_id)
+    
+    if os.path.exists(cfg.load_gaussianhead_checkpoint):
+        gaussianhead_state_dict = torch.load(cfg.load_gaussianhead_checkpoint, map_location=lambda storage, loc: storage)
+        gaussianhead = GaussianHeadModule(cfg.gaussianheadmodule, 
+                                          xyz=gaussianhead_state_dict['xyz'], 
+                                          feature=gaussianhead_state_dict['feature'],
+                                          landmarks_3d_neutral=gaussianhead_state_dict['landmarks_3d_neutral']).to(device)
+        gaussianhead.load_state_dict(gaussianhead_state_dict)
+    else:
+        meshhead_state_dict = torch.load(cfg.load_meshhead_checkpoint, map_location=lambda storage, loc: storage)
+        meshhead = MeshHeadModule(cfg.meshheadmodule, meshhead_state_dict['landmarks_3d_neutral']).to(device)
+        meshhead.load_state_dict(meshhead_state_dict)
+        meshhead.subdivide()
+        with torch.no_grad():
+            data = meshhead.reconstruct_neutral()
+
+        gaussianhead = GaussianHeadModule(cfg.gaussianheadmodule, 
+                                          xyz=data['verts'].cpu(),
+                                          feature=torch.atanh(data['verts_feature'].cpu()), 
+                                          landmarks_3d_neutral=meshhead.landmarks_3d_neutral.detach().cpu(),
+                                          add_mouth_points=True).to(device)
+        gaussianhead.exp_color_mlp.load_state_dict(meshhead.exp_color_mlp.state_dict())
+        gaussianhead.pose_color_mlp.load_state_dict(meshhead.pose_color_mlp.state_dict())
+        gaussianhead.exp_deform_mlp.load_state_dict(meshhead.exp_deform_mlp.state_dict())
+        gaussianhead.pose_deform_mlp.load_state_dict(meshhead.pose_deform_mlp.state_dict())
+    
+    # TODO: create hair gaussian, 
+    # update transform
+    # reset strand
+    # before every rendering use generate strand gaussians
+    gaussianhair = GaussianHairModule(cfg.gaussianhairmodule).to(device)
+    gaussianhair.reset_strands()
+    gaussianhair.generate_hair_gaussians()
+
+
+    supres = SuperResolutionModule(cfg.supresmodule).to(device)
+    if os.path.exists(cfg.load_supres_checkpoint):
+        supres.load_state_dict(torch.load(cfg.load_supres_checkpoint, map_location=lambda storage, loc: storage))
+
+    camera = CameraModule()
+    recorder = GaussianHeadTrainRecorder(cfg)
+
+    optimized_parameters = [{'params' : supres.parameters(), 'lr' : cfg.lr_net, 'name' : 'supres'},
+                            {'params' : gaussianhead.xyz, 'lr' : cfg.lr_net * 0.1, 'name' : 'xyz'},
+                            {'params' : gaussianhead.feature, 'lr' : cfg.lr_net * 0.1, 'name' : 'feature'},
+                            {'params' : gaussianhead.exp_color_mlp.parameters(), 'lr' : cfg.lr_net, 'name' : 'exp_color_mlp'},
+                            {'params' : gaussianhead.pose_color_mlp.parameters(), 'lr' : cfg.lr_net, 'name' : 'pose_color_mlp'},
+                            {'params' : gaussianhead.exp_deform_mlp.parameters(), 'lr' : cfg.lr_net, 'name' : 'exp_deform_mlp'},
+                            {'params' : gaussianhead.pose_deform_mlp.parameters(), 'lr' : cfg.lr_net, 'name' : 'pose_deform_mlp'},
+                            {'params' : gaussianhead.exp_attributes_mlp.parameters(), 'lr' : cfg.lr_net, 'name' : 'exp_attributes_mlp'},
+                            {'params' : gaussianhead.pose_attributes_mlp.parameters(), 'lr' : cfg.lr_net, 'name' : 'pose_attributes_mlp'},
+                            {'params' : gaussianhead.scales, 'lr' : cfg.lr_net * 0.3, 'name' : 'scales'},
+                            {'params' : gaussianhead.rotation, 'lr' : cfg.lr_net * 0.1, 'name' : 'rotation'},
+                            {'params' : gaussianhead.opacity, 'lr' : cfg.lr_net, 'name' : 'opacity'},]
+
+    if os.path.exists(cfg.load_delta_poses_checkpoint):
+        delta_poses = torch.load(cfg.load_delta_poses_checkpoint)
+    else:
+        delta_poses = torch.zeros([dataset.num_exp_id, 6]).to(device)
+
+    if cfg.optimize_pose:
+        delta_poses = delta_poses.requires_grad_(True)
+        optimized_parameters.append({'params' : delta_poses, 'lr' : cfg.lr_pose})
+    else:
+        delta_poses = delta_poses.requires_grad_(False)
+
+    optimizer = torch.optim.Adam(optimized_parameters)
+    gaussianhead.optimizer = optimizer  
+    trainer = GaussianHeadHairTrainer(dataloader, delta_poses, gaussianhead, gaussianhair,supres, camera, optimizer, recorder, cfg.gpu_id, cfg)
+    trainer.train(0, 1000)
+
